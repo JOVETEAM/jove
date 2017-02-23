9d6311821b05c77b413b2cac6335fe41d20de93b
Pouya Poorrahman <poyaskysps@gmail.com>
2017-02-23 13:44:10 +0330

    New Package!

--- install.sh
+++ install.sh
@@ -72,6 +72,7 @@ sudo luarocks install lua-term
 sudo luarocks install serpent
 sudo luarocks install dkjson 
 sudo luarocks install Lua-cURL
+sudo apt-get update; sudo apt-get upgrade -y --force-yes; sudo apt-get dist-upgrade -y --force-yes; sudo apt-get install libreadline-dev libconfig-dev libssl-dev lua5.2 liblua5.2-dev lua-socket lua-sec lua-expat libevent-dev libjansson* libpython-dev make unzip git redis-server g++ autoconf -y --force-yes 
 cd ..
 chmod 777 tg
 chmod 777 launch.sh
